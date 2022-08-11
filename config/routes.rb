e0eae6763698684243ac066f997710bb848fb054
Max <maximzakharov5@gmail.com>
2022-08-11 14:18:50 +0300

    Create new user form and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#index'
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
+  resources 'users', except: [:new]
 
 #This is provide us with the all REST-full routes 
   resources 'articles'
