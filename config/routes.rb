c002e676764606b3366c8008cc7b5db73f9796e4
Max <maximzakharov5@gmail.com>
2022-08-18 21:35:50 +0300

    Start working with the log in form

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'signup', to: 'users#new'
   resources 'users', except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
 #This is provide us with the all REST-full routes 
   resources 'articles'
