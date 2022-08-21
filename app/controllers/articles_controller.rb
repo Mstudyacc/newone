aeccd70301d40c5f4d89837cc85c0e3f2e00a5b9
Max <maximzakharov5@gmail.com>
2022-08-21 16:35:47 +0300

    Navigation partial updated to display [A] for admin user accounts. Article actions (edit and delete) made available to admins for all articles in both for articles index (rendering article partial) and articles show views. Added ability for admin users to delete other user accounts through the users index page.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -55,7 +55,7 @@ class ArticlesController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @article.user
+    if current_user != @article.user && !current_user.admin?
       flash[:alert] = "You can edit or delete only your articles!"
       redirect_to @article
     end
