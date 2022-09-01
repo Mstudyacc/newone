88cd3bfc021b00007d1d96fa51d0c9c9d826c91f
Max <maximzakharov5@gmail.com>
2022-09-01 20:29:16 +0300

    Added unit tests for various category model validation specs

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,8 +1,34 @@
 require './test/test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  test "Categoty should be valid" do 
+
+  def setup
     @category = Category.new(name: "Sports")
+  end
+  
+  test "Categoty should be valid" do 
     assert @category.valid?
   end
+
+  test "name should be present" do
+    @category.name = " "
+    assert_not @category.valid?
+  end
+
+  test "name should be uniqe" do
+    @category.save
+    @category2 = Category.new(name: "Sports")
+    assert_not @category2.valid?
+  end
+
+  test "name shouldn't be too long" do
+    @category.name = "a" * 26
+    assert_not @category.valid?
+  end
+
+  test "name shouldn;t be too short" do
+    @category.name = "a"
+    assert_not @category.valid?
+  end
+
 end
